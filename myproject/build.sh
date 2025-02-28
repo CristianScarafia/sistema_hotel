7d3441bfd1bb69ac1ffbabc3607ad10b6582204f
CristianScarafia <cris_scarafia@hotmail.com>
2025-02-28 19:37:51 -0300

    ff

--- myproject/build.sh
+++ myproject/build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-set -o errexit  # Detener en caso de error
+# Exit on error
+set -o errexit
 
-pip install --no-cache-dir -r requirements.txt
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
+
+# Convert static asset files
 python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
 python manage.py migrate
