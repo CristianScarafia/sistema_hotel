f99cfdc359c7233b0dc4bfd203050482c4c2a294
CristianScarafia <cris_scarafia@hotmail.com>
2025-02-28 20:10:02 -0300

    fixes

--- myproject/myproject/settings/production.py
+++ myproject/myproject/settings/production.py
@@ -12,13 +12,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
 
-if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    
+
 
 ALLOWED_HOSTS = [ '*' ]
 
