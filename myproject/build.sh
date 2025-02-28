4af7e698c1efc1fe2acb5a8c5511689bf614103e
CristianScarafia <cris_scarafia@hotmail.com>
2025-02-28 19:26:05 -0300

    Añadiendo script de compilación para Render

--- myproject/build.sh
+++ myproject/build.sh
@@ -1,11 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -o errexit  # Detener en caso de error
 
-echo 'Running collectstatic...'
-python manage.py collectstatic --no-input --settings=myproject.settings.production
-
-echo 'Applying migrations...'
-python manage.py wait_for_db --settings=myproject.settings.production 
-python manage.py migrate --settings=myproject.settings.production
-
-echo 'Runing server...'
-gunicorn --env DJANGO_SETTINGS_MODULE=myproject.settings.production myproject.wsgi:application --bind 0.0.0.0:8000
+pip install --no-cache-dir -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
