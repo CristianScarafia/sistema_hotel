96b9617f66a741e36b6338f36881c13dde949b00
CristianScarafia <cris_scarafia@hotmail.com>
2025-02-28 21:25:04 -0300

    dominio

--- myproject/settings/production.py
+++ myproject/settings/production.py
@@ -11,6 +11,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://sistemahotel-sistemahotel.up.railway.app"
+]
 
 
 
